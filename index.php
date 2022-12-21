e7660ad4a16cbad2ff93d121f3a4bcf619033e8f
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2022-12-21 13:52:02 +0100

    initial project

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong Password</title>
+</head>
+
+<body>
+
+    <head>
+
+        <h1>Strong Password</h1>
+
+    </head>
+
+    <main>
+        
+    </main>
+
+</body>
+
+</html>
