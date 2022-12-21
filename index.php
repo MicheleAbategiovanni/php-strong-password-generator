c83a2cca6baaa796e661c13e47814732c3bafa87
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2022-12-21 14:53:04 +0100

    completed milestone 1

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
+$passwordLenght = isset($_GET["length"]) ? $_GET["length"] : 0;
+// $passwordLenght = 5;
+$pippo = "";
+
+function randomChar($passwordLenght)
+{
+
+    $chars = "abcdefghiklmnopqrstuvwxyz";
+    $charsUp = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+    $symbol = "!@#$%^&*()-_=+[]{};:',./\|";
+    $numbers = "0123456789";
+
+    $password = $chars . $numbers . $charsUp . $symbol;
+
+    for ($i = 0; $i < $passwordLenght; $i++) {
+        $charIndex = rand(0, strlen($password) - 1);
+        echo $password[$charIndex];
+    }
+}
 
 ?>
 
@@ -12,18 +31,57 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strong Password</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
 
     <head>
+        <nav class="nav py-3">
 
-        <h1>Strong Password</h1>
+            <h1 class="text-center">Strong Password</h1>
+
+        </nav>
+
 
     </head>
 
     <main>
-        
+
+        <section>
+
+            <div class="container">
+
+                <form action="" method="get">
+
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">Lunghezza</span>
+                        <input type="text" class="form-control" placeholder="" name="length">
+                        <button class="btn btn-danger">Invia</button>
+                    </div>
+
+                </form>
+
+
+                <div>
+                    <h2>
+
+                        <?php if (isset($_GET['length'])) { ?>
+                            <div class="alert alert-warning" role="alert">
+                                <h5 class="text-black">La password generata per te è:</h5>
+                                <p class="text-danger"> <?php randomChar($passwordLenght) ?> </p>
+                            </div>
+                            <?php } ?>
+
+                    </h2>
+                </div>
+
+
+            </div>
+
+        </section>
+
     </main>
 
 </body>
